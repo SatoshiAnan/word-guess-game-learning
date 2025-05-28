c2445755e493b4995f3db3f71f9073214d1abd12
SatoshiAnan <anan.satoshi@chrono.co.jp>
2025-05-28 16:54:26 +0900

    Added comment to GameViewModelTest

--- app/src/test/java/com/example/unscramble/test/ui/GameViewModelTest.kt
+++ app/src/test/java/com/example/unscramble/test/ui/GameViewModelTest.kt
@@ -7,25 +7,29 @@ import org.junit.Assert.assertFalse
 import org.junit.Test
 
 class GameViewModelTest {
+    // Testクラス自体を再コンポーズすることはないため、GameViewModelインスタンスを直接作成
     private val viewModel = GameViewModel()
 
+    // シャッフル前の単語を取得する関数
     private fun getUnscrambledword(): String {
         return viewModel.pickWord
     }
 
     @Test
     fun gameViewModel_CorrectWordGuessed_ScoreUpdatedAndErrorFlagUnset() {
-        var currentGameUiState = viewModel.uiState.value
+        var currentGameUiState = viewModel.uiState.value // uiStateのGameUiStateインスタンスを取得
         val correctPlayerWord = getUnscrambledword()
 
         viewModel.updateUserGuess(correctPlayerWord)
         viewModel.checkUserGuess()
 
         currentGameUiState = viewModel.uiState.value
+        // 引数がFalseであることを確認
         assertFalse(currentGameUiState.isGuessedWordWrong)
+        // 引数同士が等しいことを確認
         assertEquals(SCORE_AFTER_FIRST_CORRECT_ANSWER, currentGameUiState.score)
     }
-
+    // Javaでいうstaticフィールド・関数と同義
     companion object {
         private const val SCORE_AFTER_FIRST_CORRECT_ANSWER = SCORE_INCREASE
     }
